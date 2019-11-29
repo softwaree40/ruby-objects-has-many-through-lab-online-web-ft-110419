5d28429a857561f130b43cad6ba7a0246d326a15
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 04:03:34 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,24 @@
+class Patient
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select { |a| a.patient == self }
+  end
+
+  def doctors
+    self.appointments.collect { |a| a.doctor }
+  end
+
+  def self.all
+    @@all
+  end
+end
